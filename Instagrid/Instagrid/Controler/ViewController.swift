0369cf56494cbca1f321915753e37e6f86896bbc
Fabien Dietrich <32198533+Destrarion@users.noreply.github.com>
2019-02-17 22:39:01 +0100

    Commit from Wednesday 13/02/19
    Next change incoming : Responsive and animation

--- Instagrid/Instagrid/Controler/ViewController.swift
+++ Instagrid/Instagrid/Controler/ViewController.swift
@@ -107,8 +107,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             
             print("image found")
             //do something with an image
-           self.currentButton!.setImage(image, for: .normal)
             
+           self.currentButton!.setImage(image, for: .normal)
+            self.currentButton!.imageView?.contentMode = .scaleAspectFill
+            @
             
             
         } else {
